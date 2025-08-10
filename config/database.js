9e98994b994397845b7ef6cec244c11b7648f3ef
DtgfjtwsXehpehp <sivuyilemtwetwe@gmail.com>
2025-08-10 16:30:47 +0200

    default

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
-// import mysql from 'mysql2/promise';
-// import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
 export default async function query(sql, params) {
   const connection = await mysql.createConnection({
