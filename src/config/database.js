efd4077c9b5166e713fc4506cb705c295b27b9e1
Sivuyile Mtwetwe <sivuyilemtwetwe@gmail.com>
2026-06-10 13:29:07 +0200

    Fix database config to load dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.supabase = void 0;
 const supabase_js_1 = require("@supabase/supabase-js");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const supabaseUrl = process.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;
 if (!supabaseUrl || !supabaseAnonKey) {
